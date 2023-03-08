1a2a3315f4937972282557abac71ed42da46fe55
David Youssefi <david.youssefi@cnes.fr>
2023-03-08 13:01:26 +0000

    test: add synthetic case

--- tests/test_cars_rasterize.py
+++ tests/test_cars_rasterize.py
@@ -19,30 +19,75 @@
 #
 """Tests for `cars_rasterize` package."""
 
+from typing import Any, Dict
+
+import numpy as np
+
 # Third party imports
 import pytest
 
-# cars_rasterize imports
-import cars_rasterize
+# rasterize imports
+import rasterize
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
+def synthetic_cloud() -> Dict[str, Any]:
+    """Generate synthetic point cloud."""
+    cloud = {
+        "xstart": 0.0,
+        "ystart": 0.0,
+        "xsize": 2,
+        "ysize": 2,
+        "resolution": 1.0,
+    }  # type: Dict[str, Any]
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # Example to edit
-    return "response"
+    x = np.arange(cloud["xsize"]) * cloud["resolution"]
+    x += cloud["xstart"] + cloud["resolution"] / 2
+    y = np.arange(cloud["ysize"]) * -cloud["resolution"]
+    y -= cloud["ystart"] + cloud["resolution"] / 2
+    xgrid, ygrid = np.meshgrid(x, y)
+
+    cloud["points"] = np.vstack((xgrid.ravel(), ygrid.ravel()))
+    cloud["values"] = np.arange(cloud["xsize"] * cloud["ysize"]) * 100
+    cloud["values"] = cloud["values"][np.newaxis, ...]
+    cloud["valid"] = np.ones((1, cloud["points"].shape[1]))
+    return cloud
 
 
-def test_content(response):  # pylint: disable=redefined-outer-name
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # Example to edit
-    print(response)
+@pytest.mark.parametrize("radius, sigma", [[0, 1], [0, 2], [1, np.inf]])
+def test_synthetic(
+    synthetic_cloud, radius, sigma
+):  # pylint: disable=redefined-outer-name
+    """Test on synthetic points cloud."""
+    out, mean, stdev, nb_pts_in_disc, nb_pts_in_cell = rasterize.pc_to_dsm(
+        synthetic_cloud["points"],
+        synthetic_cloud["values"],
+        synthetic_cloud["valid"],
+        synthetic_cloud["xstart"],
+        synthetic_cloud["ystart"],
+        synthetic_cloud["xsize"],
+        synthetic_cloud["ysize"],
+        synthetic_cloud["resolution"],
+        radius,
+        sigma,
+    )
 
+    if radius == 0:
+        mean_ref = np.array([[[0], [100]], [[200], [300]]])
+        out_ref = mean_ref
+        stdev_ref = np.zeros((2, 2, 1))
+        nb_pts_in_disc_ref = np.ones((2, 2))
+        nb_pts_in_cell_ref = np.ones((2, 2))
 
-def test_cars_rasterize():
-    """Sample pytest cars_rasterize module test function"""
-    assert cars_rasterize.__author__ == "CNES"
-    assert cars_rasterize.__email__ == "cars@cnes.fr"
+    elif radius == 1 and sigma == np.inf:
+        mean_ref = 150 * np.ones((2, 2, 1))
+        out_ref = mean_ref
+        stdev_ref = np.float32(50 * np.sqrt(5)) * np.ones((2, 2, 1))
+        nb_pts_in_disc_ref = np.array([[4, 4], [4, 4]])
+        nb_pts_in_cell_ref = np.array([[1, 1], [1, 1]])
+
+    assert np.array_equal(out, out_ref)
+    assert np.array_equal(mean, mean_ref)
+    assert np.array_equal(stdev, stdev_ref)
+    assert np.array_equal(nb_pts_in_disc, nb_pts_in_disc_ref)
+    assert np.array_equal(nb_pts_in_cell, nb_pts_in_cell_ref)
